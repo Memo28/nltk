41e2ae92fafefe6323b98bb6bd1e2da73ccb6a63
Memo28 <memovdg@gmail.com>
2018-02-22 21:43:22 -0600

    Seleccionador de archivos

--- Interfaz.py
+++ Interfaz.py
@@ -1,9 +1,18 @@
 import wx
+import os
+
+
+wildcard = "Text File (*.txt)|*.txt|" \
+            "All files (*.*)|*.*"
+fileSelected = "";
 
 
 class MiFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
+
+        self.currentDirectory = os.getcwd()
+
 
         # Etiquetas ...
         self.labelA = wx.StaticText(self, wx.ID_ANY, "Archivo seleccionado...", pos=(10, 15), size=(130, 15))
@@ -14,11 +23,34 @@ class MiFrame(wx.Frame):
         self.result = wx.TextCtrl(self,wx.ID_ANY,pos=(20,60),size=(450,250))
 
         # Botones
-        self.suma = wx.Button(self, wx.ID_ANY, "Abrir", pos=(350, 10), size=(80, 30))
-        self.suma = wx.Button(self, wx.ID_ANY, "Buscar", pos=(350, 320), size=(120, 30))
+        self.open = wx.Button(self, wx.ID_ANY, "Abrir", pos=(350, 10), size=(80, 30))
+        self.open.Bind(wx.EVT_BUTTON,self.OpenFile)
+        self.search = wx.Button(self, wx.ID_ANY, "Buscar", pos=(350, 320), size=(120, 30))
+        self.search.Bind(wx.EVT_BUTTON,self.SearchConcordance)
+
 
         self.Centre(True)
         self.Show()
+
+    #Abrir explorador de archivos
+    def OpenFile(self,event):
+        dlg = wx.FileDialog(
+            self, message="Seleccione un archivo",
+            defaultDir=self.currentDirectory,
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_CHANGE_DIR
+            )
+        if dlg.ShowModal() == wx.ID_OK:
+            paths = dlg.GetPaths()
+            print ("Archivo seleccionado")
+            for path in paths:
+                fileSelected = path
+                self.text.SetLabelText(fileSelected)
+        dlg.Destroy()
+
+    def SearchConcordance(self,event):
+        print("Concordance")
 
 
 if __name__ == '__main__':
