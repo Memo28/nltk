01aea9b6b7a5fc2d8a2a43887949efcbb795517d
Memo28 <memovdg@gmail.com>
2018-02-22 21:26:14 -0600

    Interfaz lista

--- Interfaz.py
+++ Interfaz.py
@@ -1,17 +1,28 @@
 import wx
 
 
-class Example(wx.Frame):
+class MiFrame(wx.Frame):
+    def __init__(self, *args, **kwargs):
+        wx.Frame.__init__(self, *args, **kwargs)
 
-    def __init__(self, parent, title):
-        super(Example, self).__init__(parent, title=title,
-                                      size=(300, 200))
+        # Etiquetas ...
+        self.labelA = wx.StaticText(self, wx.ID_ANY, "Archivo seleccionado...", pos=(10, 15), size=(130, 15))
+        self.labelB = wx.StaticText(self, wx.ID_ANY, "Resultado", pos=(200, 40), size=(80, 15))
 
-        self.Centre()
+        # Inputs
+        self.text = wx.TextCtrl(self, wx.ID_ANY, pos=(150, 10), size=(180, 25))
+        self.result = wx.TextCtrl(self,wx.ID_ANY,pos=(20,60),size=(450,250))
+
+        # Botones
+        self.suma = wx.Button(self, wx.ID_ANY, "Abrir", pos=(350, 10), size=(80, 30))
+        self.suma = wx.Button(self, wx.ID_ANY, "Buscar", pos=(350, 320), size=(120, 30))
+
+        self.Centre(True)
         self.Show()
 
 
 if __name__ == '__main__':
     app = wx.App()
-    Example(None, title='Center')
+    fr = MiFrame(None, -1, "wxPython App", size=(500, 400))
     app.MainLoop()
+
